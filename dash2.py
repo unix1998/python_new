e57821e361ca2232ddfb96ebfaa9c5e05260ac3e
Unix x 1998 <mingx_98@yahoo.com>
2024-05-11 11:33:23 -0400

    add 2 new dash example

---
+++ dash2.py
@@ -0,0 +1,24 @@
+import dash
+from dash import dcc
+from dash import html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1('Hello Dash'),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+# Run the Dash app
+app.run_server(debug=True)
